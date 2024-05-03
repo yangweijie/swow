177cbd0149f041fcecfba9f9454f036082c99b3f
twosee <twose@qq.com>
2024-05-04 05:11:10 +0800

    Make blank-fixer smarter

--- tools/blank-fixer.php
+++ tools/blank-fixer.php
@@ -15,33 +15,55 @@ declare(strict_types=1);
 (static function () use ($argv): void {
     $options = getopt('h', ['help'], $restIndex);
     $argv = array_slice($argv, $restIndex);
-    $filename = $argv[0] ?? '';
-    if (isset($options['h']) || isset($options['help']) || empty($filename)) {
+    $pattern = $argv[0] ?? '';
+    if (isset($options['h']) || isset($options['help']) || empty($pattern)) {
         $basename = basename(__FILE__);
-        exit("Usage: php {$basename} <file>\n");
+        exit("Usage: php {$basename} <pattern>\n");
     }
-    $fp = @fopen($filename, 'rb');
-    if ($fp === false) {
-        throw new RuntimeException(error_get_last()['message']);
+    // 返回多个
+    $files = glob($pattern, GLOB_BRACE);
+    if ($files === false) {
+        throw new RuntimeException('Failed to glob files');
     }
-    $lines = [];
-    while (($line = fgets($fp)) !== false) {
-        $lines[] = rtrim($line);
+    if (empty($files)) {
+        exit("No files found\n");
     }
-    $emptyLines = 0;
-    for ($n = count($lines) - 1; $n >= 0; $n--) {
-        if (empty($lines[$n])) {
-            $emptyLines++;
-        } else {
-            break;
+    $files = array_map('realpath', $files);
+    echo "Are you sure to fix the blank lines in the following files?\n";
+    foreach ($files as $file) {
+        echo "    {$file}\n";
+    }
+    echo "Type 'Y' to continue: ";
+    $line = rtrim(fgets(STDIN));
+    if ($line !== 'Y') {
+        exit("Aborted\n");
+    }
+    foreach ($files as $filename) {
+        $fp = @fopen($filename, 'rb');
+        if ($fp === false) {
+            throw new RuntimeException(error_get_last()['message']);
         }
-    }
-    $lines = array_slice($lines, 0, count($lines) - $emptyLines);
-    $lines[] = ''; // the last empty line
-    fclose($fp);
+        $lines = [];
+        while (($line = fgets($fp)) !== false) {
+            $line = rtrim($line);
+            $line = str_replace("\t", '    ', $line);
+            $lines[] = $line;
+        }
+        $emptyLines = 0;
+        for ($n = count($lines) - 1; $n >= 0; $n--) {
+            if (empty($lines[$n])) {
+                $emptyLines++;
+            } else {
+                break;
+            }
+        }
+        $lines = array_slice($lines, 0, count($lines) - $emptyLines);
+        $lines[] = ''; // the last empty line
+        fclose($fp);
 
-    $contents = implode("\n", $lines);
-    if (!@file_put_contents($filename, $contents)) {
-        throw new RuntimeException(error_get_last()['message']);
+        $contents = implode("\n", $lines);
+        if (!@file_put_contents($filename, $contents)) {
+            throw new RuntimeException(error_get_last()['message']);
+        }
     }
 })();
