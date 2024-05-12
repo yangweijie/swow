e3a22df6b489fbe4c864bb192528f64dfcee934b
twosee <twose@qq.com>
2024-05-13 03:27:01 +0800

    Back to dev

--- tools/versions.php
+++ tools/versions.php
@@ -13,10 +13,10 @@ declare(strict_types=1);
 
 return [
     'swow-extension' => [
-        'version' => '1.5.1',
+        'version' => '1.5.2-dev',
     ],
     'swow-library' => [
-        'version' => '1.5.1',
+        'version' => '1.5.2-dev',
         'required_extension_version' => '^1.5.1',
     ],
 ];
